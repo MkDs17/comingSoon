7b529c58d2421bd78799ec7fe0fb4ed02fbb5d7e
MkDs17 <mickael.rassicot@gmail.com>
2020-01-11 11:12:49 +0100

    add custom dynamic counter

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ let app = {
       console.log('init')
       
       app.randomBackground();
+
+      let deadline = 'Mar 10 2020 11:09:16';      
+      app.initializeClock(deadline);
     
     },        
   
@@ -11,12 +14,48 @@ let app = {
       let base = 'img/';
       $("#bg").backstretch([`${base}bg.jpg`, `${base}bg1.jpg`, `${base}bg2.jpg`],
       {duration: 5000, fade:1500});
-    },   
+    }, 
+
+    initializeClock: function(endtime) {      
+      let daysSpan = document.getElementById('days');
+      let hoursSpan = document.getElementById('hours');
+      let minutesSpan = document.getElementById('minutes');
+      let secondsSpan = document.getElementById('seconds');
+    
+      function updateClock() {
+        let t = app.getTimeRemaining(endtime);
+    
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+      }
+
+      updateClock();
+      let timeinterval = setInterval(updateClock, 1000);
+    },
+
+    getTimeRemaining: function(endtime) {
+      let t = Date.parse(endtime) - Date.parse(new Date());
+      /* console.log('t =', t); */
+      let seconds = Math.floor((t / 1000) % 60);
+      let minutes = Math.floor((t / 1000 / 60) % 60);
+      let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      let days = Math.floor(t / (1000 * 60 * 60 * 24));
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+    },
     
   };    
     
 $(app.init);
   
-  
-  
-    
